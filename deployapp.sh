0789291abc8fcec560968beefd33b64a570cfe64
Ubuntu <renardiarvy@gmail.com>
2021-03-27 15:45:51 +0000

    add deployment

--- deployapp.sh
+++ deployapp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Make sure your code is tested!, the next step it'll deploy to staging automaticly"
+echo "Make sure your code is tested!, the next step it'll deploy to staging and build in prod automaticly"
 
 echo -n "ARE you sure (Y/N)?"
 
@@ -11,7 +11,11 @@ while [[ ! $answered ]]; do
     if [[ $answer = [Yy] ]]; then
         git checkout staging
         git merge master
-        git push origin master
+        git push origin staging
+        git checkout prod
+        git merge staging
+        git push prod
+        git checkout master
     elif [[ $answer = [Nn] ]]; then
         answered="no"
     fi
