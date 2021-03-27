ae3558163fb8c6e3fd998966430118646565187b
Ubuntu <renardiarvy@gmail.com>
2021-03-27 16:01:46 +0000

    testttt

--- deployapp.sh
+++ deployapp.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
+development=$(git branch | grep development ) 
+
 echo "Make sure your code is tested!, the next step it'll deploy to staging and build in prod automaticly"
 
 echo -n "ARE you sure (Y/N)?"
 
 answered=
+
+if [[ -z $development ]]; then
+    git branch development
+else 
+    git checkout development
+    git merge master
+    git push origin development
+    git checkout master
+fi     
 
 while [[ ! $answered ]]; do
     read -r -n 1 answer
